7022b09cf34c14543341fa529cf103ceb14bc615
Mateus Rodrigues <devrdgs@gmail.com>
2017-08-15 22:22:14 -0300

    fixed socket.io connection

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const express = require('express'),
-      http = require('http'),
+      app = express(),
       helmet = require('helmet'),
       bodyParser = require('body-parser'),
       morgan = require('morgan'),
-      cors = require('cors'),
       port = 4000,
-      app = express(),
-      server = http.Server(app),
-      io = require('socket.io')(server);
+      server = require('http').createServer(app),
+      io = require('socket.io')().listen(server);
 
-app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.listen(port || process.env.PORT, () => console.log(`Express listening on port ${port}`));
+server.listen(port || process.env.PORT, () => console.log(`Express listening on port ${port}`));
+
+io.on('connection', socket => {
+  console.log('User connected');
+});
 
 module.exports = io;
